38d1fe3d4cad9d91b5ef34fa7f95b13424d63acd
mmdoza002 <mmdoza002@gmail.com>
2022-08-05 10:21:43 -0500

    Connections folder and Models update

---
+++ models/Cards.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Card extends Model {}
+
+Card.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      card_number:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      card_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'card',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'cards',
+    }
+  );
+  
+  module.exports = Card;
--- models/index.js
+++ models/index.js
@@ -1,12 +1,27 @@
 const Player = require('./Player');
 const Dealer = require('./Dealer');
+const Card = require('./Cards');
 
 
+Player.hasOne(Card,{
+    foreignKey: 'card_id',
+    onDelete: 'CASCADE',
+});
+
+Card.belongTo(Player,{
+    foreignKey: 'card_id',
+    onDelete: 'CASCADE',
+});
+
+Dealer.hasOne(Card,{
+    foreignKey: 'card_id',
+    onDelete: 'CASCADE',
+});
+
+Dealer.belongTo(Player,{
+    foreignKey: 'card_id',
+    onDelete: 'CASCADE',
+});
 
 
-
-
-
-
-
-module.exports = {Player, Dealer };
+module.exports = {Player, Dealer, Card };
