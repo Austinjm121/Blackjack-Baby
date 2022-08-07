0654f79adfb8dd8b9fe3942920141d66f31fc3dc
mmdoza002 <mmdoza002@gmail.com>
2022-08-07 18:47:16 -0500

    Routes on Welcome HB, Added player-routes

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,12 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { Player } = require('../models');
 
-const userData = require('./userData.json');
+const playerData = require('./playerData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  await Player.bulkCreate(playerData, {
     individualHooks: true,
     returning: true,
   });
